c0638956744a2955b238ba6d81e409503719465b
Danilo Bargen <mail@dbrgn.ch>
2015-11-23 22:09:22 +0100

    Made size configurable

--- grab.py
+++ grab.py
@@ -11,11 +11,7 @@ import PIL.Image
 import PIL.ImageDraw
 
 
-WIDTH = 640
-HEIGHT = 480
-
-
-def get_cam(print_info=True):
+def get_cam(size, print_info=True):
     """
     List all cams, return the first cam found.
     """
@@ -23,7 +19,7 @@ def get_cam(print_info=True):
     print('%d cam(s) found:' % len(cams))
     for cam in cams:
         print('- %s' % cam)
-    cam = pygame.camera.Camera(cams[0], (WIDTH, HEIGHT))
+    cam = pygame.camera.Camera(cams[0], size)
     return cam
 
 
@@ -35,44 +31,49 @@ def capture_surface(cam):
     return cam.get_image()
 
 
-def pygame_to_pil(surface):
+def pygame_to_pil(surface, size):
     """
     Convert a pygame surface to a PIL image.
     """
     stringimg = pygame.image.tostring(surface, 'RGBA')
-    return PIL.Image.frombytes('RGBA', (WIDTH, HEIGHT), stringimg)
+    return PIL.Image.frombytes('RGBA', size, stringimg)
 
 
-def add_timestamp(img):
+def add_timestamp(img, size):
     """
     Add the timestamp to the image. This works in-place.
     """
     draw = PIL.ImageDraw.Draw(img)
     timestamp = datetime.datetime.now().isoformat().replace('T', ' ').split('.')[0]
-    draw.text((10, HEIGHT - 20), timestamp, (255, 255, 255))
+    draw.text((10, size[1] - 20), timestamp, (255, 255, 255))
 
 
 if __name__ == '__main__':
 
     # Parse args
-    if len(sys.argv) != 2:
-        print('Usage: %s <webcamname>' % sys.argv[0])
+    if len(sys.argv) != 4:
+        print('Usage: %s <width> <height> <webcamname>' % sys.argv[0])
         sys.exit(1)
 
     # Initialize camera
     pygame.camera.init()
 
+    # Get size
+    width = int(sys.argv[1])
+    height = int(sys.argv[2])
+    size = (width, height)
+
     # Get the camera
-    cam = get_cam()
+    cam = get_cam(size)
 
     # Get a pygame surface
     surface = capture_surface(cam)
 
     # Convert to a PIL image
-    img = pygame_to_pil(surface)
+    img = pygame_to_pil(surface, size)
 
     # Add timestamp
-    add_timestamp(img)
+    add_timestamp(img, size)
 
     # Save image
     filename = '%s.jpg' % sys.argv[1]
