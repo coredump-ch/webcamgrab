d67160d8ccc26467f29ddac8408878647ad83bef
Axel Essbaum <axel@essbaum.com>
2016-02-16 21:28:25 +0100

    Refactor and add multi camera support

--- grab.py
+++ grab.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
 
+import os
 import sys
 import datetime
 
@@ -9,15 +10,45 @@ import PIL.Image
 import PIL.ImageDraw
 
 
-def capture_frame():
+def get_images(base_filename):
     """
-    Capture an image from the camera. Return a numpy ndarray with the image
-    data.
+    Step through cameras, capturing images.
     """
-    cam = cv2.VideoCapture(0)
+    camCounter = 0
+    frame = capture_frame(camCounter)
+    if frame is False:
+        print('Could not capture frame. Is a webcam connected?')
+        sys.exit(1)
+
+    while frame is not False:
+
+        # Convert to a PIL image
+        img = cv_to_pil(frame)
+
+        # Add timestamp
+        add_timestamp(img)
+
+        # Save image
+        filename = '%s_%d.jpg' % (base_filename, camCounter)
+        img.save(filename)
+
+        # Next camera...
+        camCounter += 1
+        frame = capture_frame(camCounter)
+
+
+def capture_frame(cam_num):
+    """
+    Capture an image from the given camera. Return a numpy ndarray with
+    the image data.
+    """
+    cam = cv2.VideoCapture(cam_num)
     retval, image = cam.read()
     cam.release()
-    return image
+    if retval is False:
+        return False
+    else:
+        return image
 
 
 def cv_to_pil(img):
@@ -36,7 +67,6 @@ def add_timestamp(img):
     timestamp = datetime.datetime.now().isoformat().replace('T', ' ').split('.')[0]
     draw.text((10, img.size[1] - 20), timestamp, (255, 255, 255))
 
-
 if __name__ == '__main__':
 
     # Parse args
@@ -44,18 +74,12 @@ if __name__ == '__main__':
         print('Usage: %s <webcamname>' % sys.argv[0])
         sys.exit(1)
 
-    # Get a pygame surface
-    frame = capture_frame()
-    if frame is None:
-        print('Could not capture frame. Is a webcam connected?')
-        sys.exit(1)
+    # Remove existing images (in case camera was removed we don't want
+    # its old image hanging around).
+    for i in range(10):
+        filename = '%s_%d.jpg' % (sys.argv[1], i)
+        if os.path.isfile(filename):
+            os.remove(filename)
 
-    # Convert to a PIL image
-    img = cv_to_pil(frame)
-
-    # Add timestamp
-    add_timestamp(img)
-
-    # Save image
-    filename = '%s.jpg' % sys.argv[1]
-    img.save(filename)
+    # Go!
+    get_images(sys.argv[1])
